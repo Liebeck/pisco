026d71c23321223ed62681519bbc46b8397a464c
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-07-20 16:21:16 +0200

    added more tests to check if codes are also correctly parsed

--- tests/parsers/corpus_parser_test.py
+++ tests/parsers/corpus_parser_test.py
@@ -30,6 +30,50 @@ class CorpusParserTest(unittest.TestCase):
                           'agreeableness': 40,
                           'conscientiousness': 46})
 
+    def test_number_of_codes(self):
+        documents = self.parser.parse(truth_file='personality.txt')
+        self.assertEqual(len(documents[0].codes), 2)
+        self.assertEqual(len(documents[1].codes), 1)
+
+    def test_ids_of_codes(self):
+        documents = self.parser.parse(truth_file='personality.txt')
+        self.assertEqual(documents[0].codes[0].id, '0')
+        self.assertEqual(documents[0].codes[1].id, '1')
+        self.assertEqual(documents[1].codes[0].id, '0')
+
+    def test_parent_ids_of_codes(self):
+        documents = self.parser.parse(truth_file='personality.txt')
+        self.assertEqual(documents[0].codes[0].parent_id, '68')
+        self.assertEqual(documents[0].codes[1].parent_id, '68')
+        self.assertEqual(documents[1].codes[0].parent_id, '0')
+
+    def test_labels_of_codes(self):
+        documents = self.parser.parse(truth_file='personality.txt')
+        self.assertEqual(documents[0].codes[0].label.__dict__,
+                         {'neuroticism': 54,
+                          'extroversion': 46,
+                          'openness': 50,
+                          'agreeableness': 40,
+                          'conscientiousness': 46})
+        self.assertEqual(documents[0].codes[1].label.__dict__,
+                         {'neuroticism': 54,
+                          'extroversion': 46,
+                          'openness': 50,
+                          'agreeableness': 40,
+                          'conscientiousness': 46})
+        self.assertEqual(documents[1].codes[0].label.__dict__,
+                         {'neuroticism': 60,
+                          'extroversion': 34,
+                          'openness': 40,
+                          'agreeableness': 40,
+                          'conscientiousness': 46})
+
+    def test_number_of_lines_in_code(self):
+        documents = self.parser.parse(truth_file='personality.txt')
+        self.assertEqual(len(documents[0].codes[0].lines), 35)
+        self.assertEqual(len(documents[0].codes[1].lines), 51)
+        self.assertEqual(len(documents[1].codes[0].lines), 53)
+
 
 if __name__ == '__main__':
     unittest.main()
