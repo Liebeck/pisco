b2e5f63de06a159aaa171fa1a4a146461af644b7
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-07-25 09:39:10 +0200

    changed parameter order

--- pisco/evaluation/evaluation.py
+++ pisco/evaluation/evaluation.py
@@ -2,12 +2,12 @@ from scipy import stats
 from sklearn.metrics import mean_squared_error
 
 
-def evaluate_pearson(y_predicted, y_truth):
-    return stats.pearsonr(y_predicted, y_truth)[0]
+def evaluate_pearson(y_truth, y_predicted):
+    return stats.pearsonr(y_truth, y_predicted)[0]
 
 
-def evaluate_mse(y_predicted, y_truth):
-    return mean_squared_error(y_predicted, y_truth)
+def evaluate_mse(y_truth, y_predicted):
+    return mean_squared_error(y_truth, y_predicted)
 
 
 print evaluate_pearson([1, 4, 5], [9, 6, 5])
