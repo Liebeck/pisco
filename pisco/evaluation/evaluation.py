adbe03133c464d65b54a2815d60924bd92c2566e
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-07-25 09:56:04 +0200

    Added MSE and pearson as scoring functions for the crossvalidation

--- pisco/evaluation/evaluation.py
+++ pisco/evaluation/evaluation.py
@@ -1,5 +1,14 @@
 from scipy import stats
 from sklearn.metrics import mean_squared_error
+from sklearn.metrics import make_scorer
+
+
+def pearson_scoring():
+    return make_scorer(evaluate_pearson, greater_is_better=True)
+
+
+def mse_scoring():
+    return make_scorer(evaluate_mse, greater_is_better=False)
 
 
 def evaluate_pearson(y_truth, y_predicted):
@@ -9,7 +18,6 @@ def evaluate_pearson(y_truth, y_predicted):
 def evaluate_mse(y_truth, y_predicted):
     return mean_squared_error(y_truth, y_predicted)
 
-
-print evaluate_pearson([1, 4, 5], [9, 6, 5])
-print evaluate_pearson([1, 8, 5], [9, 6, 5])
-print evaluate_mse([1, 8, 5], [9, 6, 5])
+# print evaluate_pearson([1, 4, 5], [9, 6, 5])
+# print evaluate_pearson([1, 8, 5], [9, 6, 5])
+# print evaluate_mse([1, 8, 5], [9, 6, 5])
--- start.py
+++ start.py
@@ -1,5 +1,7 @@
 import numpy as np
 from operator import itemgetter
+
+
 def report(grid_scores, n_top=3):
     top_scores = sorted(grid_scores, key=itemgetter(1), reverse=True)[:n_top]
     for i, score in enumerate(top_scores):
@@ -14,7 +16,8 @@ def report(grid_scores, n_top=3):
 from pisco.pipeline import pipeline
 from pisco.transformers import unigram
 from pisco.loaders.plain_loader import load
-X,Y = load(level='code', labels=['openness'])
+
+X, Y = load(level='code', labels=['openness'])
 Y = [y[0] for y in Y]
 transformers = [unigram.unigram()]
 p = pipeline.pipeline(transformers)
@@ -22,11 +25,20 @@ p.fit(X, Y)
 result = p.predict(X)
 print('Success!')
 
-
 from sklearn.grid_search import GridSearchCV
 
 param_grid = {'svm__C': [1e3, 5e3, 1e4, 5e4, 1e5], 'svm__gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1]}
 
-grid_search = GridSearchCV(p, param_grid=param_grid, verbose=10, cv=3, n_jobs=4)
-grid_search.fit(X,Y)
+from pisco.evaluation.evaluation import mse_scoring
+from pisco.evaluation.evaluation import pearson_scoring
+
+evaluation_score = "pearson"
+scoring_function = ""
+if evaluation_score == "mse":
+    scoring_function = mse_scoring
+else:
+    scoring_function = pearson_scoring
+
+grid_search = GridSearchCV(p, param_grid=param_grid, verbose=10, cv=3, n_jobs=4, scoring=scoring_function)
+grid_search.fit(X, Y)
 report(grid_search.grid_scores_)
