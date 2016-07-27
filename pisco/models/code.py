77a948a837f562e4db91c6f5153168136aaa0d8e
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-07-27 16:39:16 +0200

    removed the code model

--- pisco/models/code.py
+++
@@ -1,6 +0,0 @@
-class Code():
-    def __init__(self, id=None, parent_id=None, lines=[], label=None):
-        self.id = id
-        self.parent_id = parent_id
-        self.lines = lines
-        self.label = label
