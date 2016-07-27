8a82d59f54b9a68fa485530ca4de33b960b6b7fc
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-07-27 15:01:28 +0200

    no manipulation of the inputs in transformers

--- pisco/transformers/unigram.py
+++ pisco/transformers/unigram.py
@@ -1,19 +1,8 @@
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.preprocessing import Normalizer
 from sklearn.pipeline import Pipeline
-from ..functiontransformers import functiontransformers
-from sklearn.preprocessing import FunctionTransformer
 from sklearn.pipeline import make_pipeline
 
 
 def unigram():
-    vectorizer = CountVectorizer(min_df=2, ngram_range=(1, 1))
-    # pipeline = Pipeline(steps=[('functiontransform', FunctionTransformer(functiontransformers.all_code_lines)),
-    # ('vect', vectorizer)])
-    # pipeline = Pipeline([('vect', vectorizer)])
-    pipeline = make_pipeline(
-        vectorizer,
-        FunctionTransformer(functiontransformers.all_code_lines, accept_sparse=True),
-    )
-
+    pipeline = Pipeline([('vec', CountVectorizer(min_df=2, ngram_range=(1,1)))])
     return ('unigram', pipeline)
