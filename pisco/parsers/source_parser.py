140b58ea082c12a9a9655462a6d91e6974076f48
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-07-28 12:08:40 +0200

    added the functionality to keep return only parsable code segments to the source parser

--- pisco/parsers/source_parser.py
+++ pisco/parsers/source_parser.py
@@ -1,3 +1,5 @@
+import javalang
+import logging
 import sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
@@ -7,5 +9,16 @@ class SourceParser():
     def __init__(self, splitter=u"<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>\r"):
         self.splitter = splitter
 
-    def parse(self, x):
-        return x.split(self.splitter)
+    def parse(self, x, only_parsable=False):
+        codes = []
+        segments = x.split(self.splitter)
+        if only_parsable:
+            for segment in segments:
+                try:
+                    javalang.parse.parse(segment)
+                    codes.append(segment)
+                except Exception as e:
+                    logging.warn('Parsing java code failed: '.format(e))
+            return codes
+        else:
+            return segments
