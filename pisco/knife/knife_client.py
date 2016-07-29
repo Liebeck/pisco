e2befc9890ddee3c22f5d2094fff8a370b6618b7
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-07-29 17:08:42 +0200

    fixed some bugs in knife client

--- pisco/knife/knife_client.py
+++ pisco/knife/knife_client.py
@@ -7,7 +7,12 @@ class KnifeClient:
     def __init__(self, url='http://0.0.0.0:4567/'):
         self.url = url
 
-    def extract_method_blocks(self, clazz=None):
-        r = requests.post(os.path.join(self.url, 'method/blocks'),
+    def method_blocks(self, clazz=None):
+        r = requests.post(os.path.join(self.url, 'extract'),
                           data={'class': clazz})
-        return r.json()['methodBlocks']
+        if r.json()['state'] == 'OK':
+            all_methods = map(lambda clazz: clazz['methods'],r.json()['classes'])
+            method_blocks = map(lambda class_methods: map(lambda m: m['codeBlock'], class_methods),all_methods)
+            return method_blocks
+        else:
+            return None
