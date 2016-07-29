1c33543f34ab965ed309450e5521a921d5ed05ee
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-07-29 12:36:46 +0200

    experimenting with function level features

---
+++ pisco/transformers/function_level.py
@@ -0,0 +1,51 @@
+from __future__ import unicode_literals
+from sklearn.base import BaseEstimator
+from parsers.source_parser import SourceParser
+from sklearn.pipeline import Pipeline
+from ..knife.knife_client import KnifeClient
+import numpy as np
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+
+def class_level():
+    pipeline = Pipeline([('mean_num_functions_per_class', ClassLevelTransformer())])
+    return ('class_level', pipeline)
+
+
+class ClassLevelTransformer(BaseEstimator):
+    def __init__(self, mean_function_length=True):
+        self.mean_function_length = mean_function_length
+        self.parser = SourceParser()
+        self.client = KnifeClient()
+
+    def get_feature_names(self):
+        return np.array(['mean_num_function_per_class'])
+
+    def fit(self, documents, y=None):
+        return self
+
+    def transform(self, X):
+        list_class_list = extract_classes(X)
+        result = []
+        for x in list_class_list:
+            num_functions_per_class = self._transform(x)
+            mean_num_functions_per_class = 10 * sum(num_functions_per_class) / len(num_functions_per_class)
+            result.append(mean_num_functions_per_class)
+        return result
+
+
+    def _transform(self, x):
+        num_functions_per_class = []
+        for clazz in x:
+            functions = self.client.extract_method_blocks(clazz)
+            num_functions_per_class.append(len(functions))
+        return num_functions_per_class
+
+
+def extract_classes(X):
+    list_class_list = []
+    for x in X:
+        class_list = x.split(u"<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>\r")
+        list_class_list.append(class_list)
