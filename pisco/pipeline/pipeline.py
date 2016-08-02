3aefafb5ad4d1e3cec33d5ecdb064cb6b88441c3
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-08-02 13:33:27 +0200

    simplified the pipeline

--- pisco/pipeline/pipeline.py
+++ pisco/pipeline/pipeline.py
@@ -2,11 +2,5 @@ from sklearn.pipeline import Pipeline, FeatureUnion
 from ..recognizers.linear_regression import linear_regression
 
 
-def pipeline(transformers, classifier=linear_regression()):
-    steps = []
-    transformer_list = []
-    for transformer in transformers:
-        transformer_list.append(transformer)
-    steps.append(("union", FeatureUnion(transformer_list)))
-    steps.append(classifier)
-    return Pipeline(steps)
+def pipeline(transformers=None, recognizer=None):
+    return Pipeline(steps=[('union', FeatureUnion(transformers)), recognizer])
