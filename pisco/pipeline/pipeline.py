e266eef85aac8973e590630f495d5a44433f5f80
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-08-02 11:01:56 +0200

    refactored the pipeline to include only one classifier

--- pisco/pipeline/pipeline.py
+++ pisco/pipeline/pipeline.py
@@ -1,16 +1,13 @@
 from sklearn.pipeline import Pipeline, FeatureUnion
-from sklearn.pipeline import Pipeline
-from sklearn.svm import SVC
+from ..recognizers.linear_regression import linear_regression
 
 
-def pipeline(transformers):
+def pipeline(transformers, classifier=linear_regression()):
     steps = []
     feature_union = []
     for transformer in transformers:
         feature_union.append(transformer)
     feature_union_1 = FeatureUnion(feature_union)
     steps.append(("fu", feature_union_1))
-    classifiers = [('svm', SVC())]
-    for classifier in classifiers:
-        steps.append(classifier)
+    steps.append(classifier)
     return Pipeline(steps)
