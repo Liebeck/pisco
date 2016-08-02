76afb6e21e89f294ec80de420f7def8bb5bfec42
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-08-02 14:51:50 +0200

    using char analyzer

--- pisco/transformers/unigram.py
+++ pisco/transformers/unigram.py
@@ -13,9 +13,9 @@ def ngram_ranges(begin, end):
 
 def param_grid():
     return {'union__unigram__vec__lowercase': [True, False],
-            'union__unigram__vec__ngram_range': ngram_ranges(1,5)}
+            'union__unigram__vec__ngram_range': ngram_ranges(2,5)}
 
 
 def unigram():
-    pipeline = Pipeline([('vec', CountVectorizer(ngram_range=(2, 5)))])
+    pipeline = Pipeline([('vec', CountVectorizer(analyzer='char', ngram_range=(2, 5)))])
     return ('unigram', pipeline)
