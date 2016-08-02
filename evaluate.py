da21398bbe084e64227630fe123b106d1b07142d
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-08-02 13:55:08 +0200

    minor modification of evaluate script

--- evaluate.py
+++ evaluate.py
@@ -8,6 +8,28 @@ import argparse
 import logging
 
 
+
+def config_argparser():
+    argparser = argparse.ArgumentParser(description='Personality Recognition Evaluation')
+    argparser.add_argument('-l', '--log-level', dest='log_level', type=str, default='INFO',
+                           help='Set log level (DEBUG, INFO, ERROR)')
+
+    argparser.add_argument('-c', '--train_corpus', dest='training_corpus', type=str, required=True,
+                           help='Set name of the training corpus used for the evaluation: ' + pretty_list(
+                               conf.get_dataset_names()))
+
+    argparser.add_argument('-t', '--test_corpus', dest='test_corpus', type=str, required=False,
+                           help='Set name of the test corpus used for the evaluation: ' + pretty_list(
+                               conf.get_dataset_names()))
+
+    argparser.add_argument('-r', '--recognizer', dest='recognizer_name', type=str, required=True,
+                           help='Name of the invoked recognizer: ' + pretty_list(conf.get_recognizer_names()))
+
+    argparser.add_argument('-f', '--features', dest='feature_names', type=str, required=True,
+                           help='Name of the features: ' + pretty_list(conf.get_feature_names()))
+    return argparser
+
+
 def configure(conf):
     @conf.dataset('neuroticism', labels=['neuroticism'])
     @conf.dataset('extroversion', labels=['extroversion'])
@@ -33,30 +55,17 @@ def pretty_list(items):
 
 if __name__ == '__main__':
     conf = Configuration()
-    argparser = argparse.ArgumentParser(description='Personality Recognition Evaluation')
-    argparser.add_argument('-l', '--log-level', dest='log_level', type=str, default='INFO',
-                           help='Set log level (DEBUG, INFO, ERROR)')
 
-    argparser.add_argument('-c', '--train_corpus', dest='training_corpus', type=str, required=True,
-                           help='Set name of the training corpus used for the evaluation: ' + pretty_list(
-                               conf.get_dataset_names()))
-
-    argparser.add_argument('-t', '--test_corpus', dest='test_corpus', type=str, required=False,
-                           help='Set name of the test corpus used for the evaluation: ' + pretty_list(
-                               conf.get_dataset_names()))
-
-    argparser.add_argument('-r', '--recognizer', dest='recognizer_name', type=str, required=True,
-                           help='Name of the invoked recognizer: ' + pretty_list(conf.get_recognizer_names()))
-
-    argparser.add_argument('-f', '--features', dest='feature_names', type=str, required=True,
-                           help='Name of the features: ' + pretty_list(conf.get_feature_names()))
-
+    argparser = config_argparser()
     args = argparser.parse_args()
+
     LOGFMT = '%(asctime)s %(name)s %(levelname)s %(message)s'
     logging.basicConfig(level=getattr(logging, args.log_level), format=LOGFMT)
 
     configure(conf)
+
     X_train, y_train = conf.get_dataset(args.training_corpus)
-    recognizer_instance = conf.get_recognizer(args.recognizer_name)
+    recognizer = conf.get_recognizer(args.recognizer_name)
     features = conf.get_feature(args.feature_names)
-    benchmark(X_train, y_train, recognizer_instance, features)
+
+    benchmark(X_train, y_train, recognizer, features)
