1734ce5c4c2b8ac40b3349f18122f26e6831f5c9
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-08-02 12:43:05 +0200

    refactored the pipeline

--- pisco/pipeline/pipeline.py
+++ pisco/pipeline/pipeline.py
@@ -4,10 +4,9 @@ from ..recognizers.linear_regression import linear_regression
 
 def pipeline(transformers, classifier=linear_regression()):
     steps = []
-    feature_union = []
+    transformer_list = []
     for transformer in transformers:
-        feature_union.append(transformer)
-    feature_union_1 = FeatureUnion(feature_union)
-    steps.append(("fu", feature_union_1))
+        transformer_list.append(transformer)
+    steps.append(("union", FeatureUnion(transformer_list)))
     steps.append(classifier)
     return Pipeline(steps)
