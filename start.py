e1bda47431fb88fa65a4d3e1c1191aea61dbd8d2
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-08-02 10:40:15 +0200

    removed unnecessary line from start script

--- start.py
+++ start.py
@@ -4,7 +4,7 @@ from pisco.loaders.plain_loader import load
 
 # Load files
 X, Y = load(labels=['openness'])
-Y = [y[0] for y in Y]
+# Y = [y[0] for y in Y]
 transformers = [class_level()]
 p = pipeline.pipeline(transformers)
 
