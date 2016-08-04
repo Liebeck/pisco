f3edbb0f7279a67255db51744be1fd914e3a5cb3
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-08-04 17:19:09 +0200

    Add unit test for get_num_functions_per_class

--- tests/classlevel_test.py
+++ tests/classlevel_test.py
@@ -1,4 +1,5 @@
 from pisco.transformers.class_level import get_number_of_classes
+from pisco.transformers.class_level import get_num_functions_per_class
 import unittest
 import json
 
@@ -12,6 +13,14 @@ class ClassLevelFeaturesTest(unittest.TestCase):
         with open('tests/knife_results/knife_result2.txt') as result2:
             knifeReponses.append(json.load(result2))
         self.assertEqual(get_number_of_classes(knifeReponses), 3)
+
+    def test_get_num_functions_per_class(self):
+        knifeReponses = []
+        with open('tests/knife_results/knife_result1.txt') as result1:
+            knifeReponses.append(json.load(result1))
+        with open('tests/knife_results/knife_result2.txt') as result2:
+            knifeReponses.append(json.load(result2))
+        self.assertEqual(get_num_functions_per_class(knifeReponses), [2, 1])
 
 
 if __name__ == '__main__':
