a2ef0fb7b9cad71978ef70d8d2a55a5ad139d2a2
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-08-05 11:10:30 +0200

    Remove unittest.skip after setting environment variables in Travis

--- tests/classlevel_test.py
+++ tests/classlevel_test.py
@@ -6,7 +6,6 @@ import json
 
 
 class ClassLevelFeaturesTest(unittest.TestCase):
-    @unittest.skip("testing skipping")
     def load_first_two_results(self):
         knife_reponses = []
         with open('tests/knife_results/knife_result1.txt') as result1:
@@ -15,7 +14,6 @@ class ClassLevelFeaturesTest(unittest.TestCase):
             knife_reponses.append(json.load(result2))
         return knife_reponses
 
-    @unittest.skip("testing skipping")
     def test_get_number_of_classes(self):
         knife_reponses = []
         with open('tests/knife_results/knife_result1.txt') as result1:
@@ -25,12 +23,10 @@ class ClassLevelFeaturesTest(unittest.TestCase):
             knife_reponses.append(json.load(result2))
         self.assertEqual(get_number_of_classes(knife_reponses), 3)
 
-    @unittest.skip("testing skipping")
     def test_get_num_functions_per_class(self):
         knife_reponses = self.load_first_two_results()
         self.assertEqual(get_num_functions_per_class(knife_reponses), [2, 1])
 
-    @unittest.skip("testing skipping")
     def test_get_percentage_class_is_private(self):
         knife_reponses = self.load_first_two_results()
         self.assertEqual(get_percentage_class_is_private(knife_reponses), 0)
