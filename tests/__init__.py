37d8cbdb431d04cf6fa6c9b32559443d3df50918
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-08-08 11:15:49 +0200

    defined some global variables in the init file of the tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,17 @@
 from pisco.transformers.structure.number_of_methods_per_class import NumberOfMethodsPerClass  # noqa
+from pisco.transformers.style.length_of_methods_per_class import LengthOfMethodsPerClass  # noqa
 
 mean_number_of_methods_per_class = NumberOfMethodsPerClass(method='mean')
 min_number_of_methods_per_class = NumberOfMethodsPerClass(method='min')
 max_number_of_methods_per_class = NumberOfMethodsPerClass(method='max')
 variance_number_of_methods_per_class = NumberOfMethodsPerClass(method='variance')  # noqa
+
+mean_lines_of_methods_per_class = NumberOfMethodsPerClass(stat='mean', method='lines')  # noqa
+min_lines_of_methods_per_class = NumberOfMethodsPerClass(stat='min', method='lines')  # noqa
+max_lines_of_methods_per_class = NumberOfMethodsPerClass(stat='max', method='lines')  # noqa
+variance_lines_of_methods_per_class = NumberOfMethodsPerClass(stat='variance', method='lines')  # noqa
+
+mean_chars_of_methods_per_class = NumberOfMethodsPerClass(stat='mean', method='char')  # noqa
+min_chars_of_methods_per_class = NumberOfMethodsPerClass(stat='min', method='char')  # noqa
+max_chars_of_methods_per_class = NumberOfMethodsPerClass(stat='max', method='char')  # noqa
+variance_chars_of_methods_per_class = NumberOfMethodsPerClass(stat='variance', method='char')  # noqa
