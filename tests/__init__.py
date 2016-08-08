f2bcb0ef308e3c9d7b7b8110402275a9f6bb1b55
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-08-08 11:20:37 +0200

    fixed typo again

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,12 +6,12 @@ min_number_of_methods_per_class = NumberOfMethodsPerClass(method='min')
 max_number_of_methods_per_class = NumberOfMethodsPerClass(method='max')
 variance_number_of_methods_per_class = NumberOfMethodsPerClass(method='variance')  # noqa
 
-mean_lines_of_methods_per_class = NumberOfMethodsPerClass(stat='mean', method='lines')  # noqa
-min_lines_of_methods_per_class = NumberOfMethodsPerClass(stat='min', method='lines')  # noqa
-max_lines_of_methods_per_class = NumberOfMethodsPerClass(stat='max', method='lines')  # noqa
-variance_lines_of_methods_per_class = NumberOfMethodsPerClass(stat='variance', method='lines')  # noqa
+mean_lines_of_methods_per_class = LengthOfMethodsPerClass(stat='mean', method='lines')  # noqa
+min_lines_of_methods_per_class = LengthOfMethodsPerClass(stat='min', method='lines')  # noqa
+max_lines_of_methods_per_class = LengthOfMethodsPerClass(stat='max', method='lines')  # noqa
+variance_lines_of_methods_per_class = LengthOfMethodsPerClass(stat='variance', method='lines')  # noqa
 
-mean_chars_of_methods_per_class = NumberOfMethodsPerClass(stat='mean', method='chars')  # noqa
-min_chars_of_methods_per_class = NumberOfMethodsPerClass(stat='min', method='chars')  # noqa
-max_chars_of_methods_per_class = NumberOfMethodsPerClass(stat='max', method='chars')  # noqa
-variance_chars_of_methods_per_class = NumberOfMethodsPerClass(stat='variance', method='chars')  # noqa
+mean_chars_of_methods_per_class = LengthOfMethodsPerClass(stat='mean', method='chars')  # noqa
+min_chars_of_methods_per_class = LengthOfMethodsPerClass(stat='min', method='chars')  # noqa
+max_chars_of_methods_per_class = LengthOfMethodsPerClass(stat='max', method='chars')  # noqa
+variance_chars_of_methods_per_class = LengthOfMethodsPerClass(stat='variance', method='chars')  # noqa
