8c089c928cbaeb3a7733d5601095fb415fe8ca45
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-08-13 11:16:15 +0200

    removed a test from classlevel_test.py

--- tests/transformers/misc/classlevel_test.py
+++ tests/transformers/misc/classlevel_test.py
@@ -1,4 +1,3 @@
-from pisco.transformers.misc.class_level import get_num_functions_per_class
 from pisco.transformers.misc.class_level import get_percentage_class_is_private
 import unittest
 import json
@@ -12,10 +11,6 @@ class ClassLevelFeaturesTest(unittest.TestCase):
         with open('tests/knife_results/knife_result2.txt') as result2:
             knife_reponses.append(json.load(result2))
         return knife_reponses
-
-    def test_get_num_functions_per_class(self):
-        knife_reponses = self.load_first_two_results()
-        self.assertEqual(get_num_functions_per_class(knife_reponses), [2, 1])
 
     def test_get_percentage_class_is_private(self):
         knife_reponses = self.load_first_two_results()
