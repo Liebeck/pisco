5da36271e2f2a80fafb1b9a09d7a30a495551502
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-08-13 11:06:09 +0200

    rewrote the feature number of comments per class

---
+++ pisco/transformers/style/number_of_comments_per_class.py
@@ -0,0 +1,38 @@
+from ..helpers import extract_sections, get_stat_function
+from ..helpers import powerset
+from sklearn.base import BaseEstimator
+import pisco.knife.adapters as adapter
+from sklearn.pipeline import Pipeline
+import numpy as np
+
+
+def build(stat='mean', types=['block', 'lines']):
+    pipeline = Pipeline([('transformer',
+                          NumberOfCommentsPerClass(stat=stat, types=types))])
+    return ('number_of_comments_per_class', pipeline)
+
+
+def param_grid():
+    return {'union__number_of_comments_per_class__transformer__stat':
+            ['mean', 'max', 'min', 'variance'],
+            'union__number_of_comments_per_class__transformer__types':
+            powerset(['block', 'line', 'javadoc'])}
+
+
+class NumberOfCommentsPerClass(BaseEstimator):
+    def __init__(self, stat='mean', types=['lines']):
+        self.stat = stat
+        self.types = types
+
+    def fit(self, submissions, y):
+        return self
+
+    def transform(self, raw_submissions):
+        return map(lambda x: self._transform(x), raw_submissions)
+
+    def _transform(self, raw_submission):
+        stat = get_stat_function(self.stat)
+        sections = extract_sections(raw_submission)
+        cs = adapter.comments(sections, self.types)
+        return stat(np.array(map(lambda x:
+                                 map(lambda x: len(x), x), cs)), axis=0)
