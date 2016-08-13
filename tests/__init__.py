b0e859b9868db720ec0d9fb39f4d76a7f511bd47
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-08-13 11:04:11 +0200

    removed unused statements from __init__.py in tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,17 +1,10 @@
 from pisco.transformers.structure.number_of_methods_per_class import NumberOfMethodsPerClass  # noqa
 from pisco.transformers.style.length_of_methods_per_class import LengthOfMethodsPerClass  # noqa
+from pisco.transformers.style.number_of_comments_per_class import NumberOfCommentsPerClass  # noqa
 
 mean_number_of_methods_per_class = NumberOfMethodsPerClass(stat='mean')
-min_number_of_methods_per_class = NumberOfMethodsPerClass(stat='min')
-max_number_of_methods_per_class = NumberOfMethodsPerClass(stat='max')
-variance_number_of_methods_per_class = NumberOfMethodsPerClass(stat='variance')  # noqa
 
 mean_lines_of_methods_per_class = LengthOfMethodsPerClass(stat='mean', method='lines')  # noqa
-min_lines_of_methods_per_class = LengthOfMethodsPerClass(stat='min', method='lines')  # noqa
-max_lines_of_methods_per_class = LengthOfMethodsPerClass(stat='max', method='lines')  # noqa
-variance_lines_of_methods_per_class = LengthOfMethodsPerClass(stat='variance', method='lines')  # noqa
-
 mean_chars_of_methods_per_class = LengthOfMethodsPerClass(stat='mean', method='chars')  # noqa
-min_chars_of_methods_per_class = LengthOfMethodsPerClass(stat='min', method='chars')  # noqa
-max_chars_of_methods_per_class = LengthOfMethodsPerClass(stat='max', method='chars')  # noqa
-variance_chars_of_methods_per_class = LengthOfMethodsPerClass(stat='variance', method='chars')  # noqa
+
+mean_number_of_comments_per_class = NumberOfCommentsPerClass(stat='mean', types=['block', 'line', 'javadoc'])  # noqa
