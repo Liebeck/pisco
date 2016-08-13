e755b42c5169d42a24ad341a4aaa333535799744
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-08-13 11:04:39 +0200

    removed deprecated tests from classlevel_test

--- tests/transformers/misc/classlevel_test.py
+++ tests/transformers/misc/classlevel_test.py
@@ -1,7 +1,5 @@
-from pisco.transformers.misc.class_level import get_number_of_classes
 from pisco.transformers.misc.class_level import get_num_functions_per_class
 from pisco.transformers.misc.class_level import get_percentage_class_is_private
-from pisco.transformers.misc.class_level import get_mean_length_of_functions_per_class
 import unittest
 import json
 
@@ -15,15 +13,6 @@ class ClassLevelFeaturesTest(unittest.TestCase):
             knife_reponses.append(json.load(result2))
         return knife_reponses
 
-    def test_get_number_of_classes(self):
-        knife_reponses = []
-        with open('tests/knife_results/knife_result1.txt') as result1:
-            knife_reponses.append(json.load(result1))
-        self.assertEqual(get_number_of_classes(knife_reponses), 2)
-        with open('tests/knife_results/knife_result2.txt') as result2:
-            knife_reponses.append(json.load(result2))
-        self.assertEqual(get_number_of_classes(knife_reponses), 3)
-
     def test_get_num_functions_per_class(self):
         knife_reponses = self.load_first_two_results()
         self.assertEqual(get_num_functions_per_class(knife_reponses), [2, 1])
@@ -31,11 +20,6 @@ class ClassLevelFeaturesTest(unittest.TestCase):
     def test_get_percentage_class_is_private(self):
         knife_reponses = self.load_first_two_results()
         self.assertEqual(get_percentage_class_is_private(knife_reponses), 0)
-
-    @unittest.skip("")
-    def test_get_mean_length_of_functions_per_class(self):
-        knife_reponses = self.load_first_two_results()
-        self.assertEqual(get_mean_length_of_functions_per_class(knife_reponses), 128 / 3)
 
 
 if __name__ == '__main__':
