991eeefb0796ab74afaa96a4cb4aebec3584d5be
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-08-14 21:56:15 +0200

    removed helper function to get access modifiers from json_helper.py

--- pisco/knife/json_helper.py
+++ pisco/knife/json_helper.py
@@ -6,12 +6,3 @@ def get_classes(response):
 def get_methods_in_clazz(clazz):
     methods = map(lambda method: method, clazz['methods'])
     return methods
-
-
-# return a list of boolean for each class, bool = class is access_modifier
-def get_clazzes_is_access_modifier(response, access_modifier):
-    classes = get_classes(response)
-    is_access_modifier_by_clazz = map(lambda clazz:
-                                      clazz[access_modifier],
-                                      classes)
-    return is_access_modifier_by_clazz
