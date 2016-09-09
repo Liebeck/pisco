7eb4097a91a5932240e3158ec8d373d671ef9c90
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-09-09 14:13:15 +0200

    added test case for external library usage ratio

---
+++ tests/transformers/structure/ratio_of_external_libraries_test.py
@@ -0,0 +1,25 @@
+from tests import ratio_of_external_libraries
+import unittest
+import codecs
+
+
+def read_submission(filename):
+    with codecs.open(filename, 'r', encoding='ISO-8859-1') as f:
+        return f.read()
+
+submission1 = read_submission('tests/resources/submission3.txt')
+submission2 = read_submission('tests/resources/submission4.txt')
+
+
+class RatioOfExternalLibrariesTest(unittest.TestCase):
+    def setUp(self):
+        self.transformer = ratio_of_external_libraries
+        self.submissions = [submission1, submission2]
+
+    def test_feature_extraction(self):
+        self.assertEqual(self.transformer.transform(self.submissions),
+                         [[1.0/3], [0.5]])
+
+
+if __name__ == '__main__':
+    unittest.main()
