5e42df3aa7f51b258c837b39aa8259c9eb3f2897
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-09-09 14:12:49 +0200

    updated the init file of the tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,6 @@
 from pisco.transformers.structure.number_of_methods_per_class import NumberOfMethodsPerClass  # noqa
 from pisco.transformers.structure.ratio_of_class_access_modifiers import RatioOfClassAccessModifiers  # noqa
+from pisco.transformers.structure.ratio_of_external_libraries import RatioOfExternalLibraries  # noqa
 from pisco.transformers.style.length_of_methods_per_class import LengthOfMethodsPerClass  # noqa
 from pisco.transformers.style.number_of_comments_per_class import NumberOfCommentsPerClass  # noqa
 
@@ -13,3 +14,5 @@ mean_number_of_comments_per_class = NumberOfCommentsPerClass(stat='mean', types=
 ratio_of_public_class_modifiers = RatioOfClassAccessModifiers(modifier='public')  # noqa
 ratio_of_private_class_modifiers = RatioOfClassAccessModifiers(modifier='private')  # noqa
 ratio_of_static_class_modifiers = RatioOfClassAccessModifiers(modifier='static')  # noqa
+
+ratio_of_external_libraries = RatioOfExternalLibraries()
