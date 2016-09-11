9653244d9069284416b28cd47a4b7349f6a22fd0
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-09-11 22:58:40 +0200

    Add unit tests for number of empty classes feature, #68

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,6 +4,7 @@ from pisco.transformers.structure.ratio_of_external_libraries import RatioOfExte
 from pisco.transformers.structure.number_of_function_parameters_per_class import NumberOfFunctionParametersPerClass  # noqa
 from pisco.transformers.structure.function_name_length import FunctionNameLength  # noqa
 from pisco.transformers.structure.function_parameter_name_length import FunctionParameterNameLength  # noqa
+from pisco.transformers.structure.number_of_empty_classes import NumberOfEmptyClases  #noqa
 from pisco.transformers.style.length_of_methods_per_class import LengthOfMethodsPerClass  # noqa
 from pisco.transformers.style.number_of_comments_per_class import NumberOfCommentsPerClass  # noqa
 
@@ -21,6 +22,7 @@ ratio_of_static_class_modifiers = RatioOfClassAccessModifiers(modifier='static')
 number_of_function_parameters_per_class = NumberOfFunctionParametersPerClass(stat='mean')  # noqa
 function_name_length = FunctionNameLength(stat='mean')  # noqa
 function_parameter_name_length = FunctionParameterNameLength(stat='mean')  # noqa
+number_of_empty_classes = NumberOfEmptyClases(stat='sum')  # noqa
 
 
 ratio_of_external_libraries = RatioOfExternalLibraries()
---
+++ tests/transformers/structure/number_of_empty_classes_test.py
@@ -0,0 +1,31 @@
+from tests import number_of_empty_classes
+import unittest
+import codecs
+
+
+def read_submission(filename):
+    with codecs.open(filename, 'r', encoding='ISO-8859-1') as f:
+        return f.read()
+
+submission5 = read_submission('tests/resources/submission5.txt')
+submission3 = read_submission('tests/resources/submission3.txt')
+submission6 = read_submission('tests/resources/submission6.txt')
+
+
+
+class NumberOfEmptyClassesTest(unittest.TestCase):
+    def setUp(self):
+        self.transformer = number_of_empty_classes
+        self.submissions = [submission5, submission3, submission6]
+
+    def test_feature_extraction(self):
+        # returns the following counts:
+        # 1) [[0], [1]] -> 1
+        # 2) [[0, 0], [0]] -> 0
+        # 3) [[0], [0]] -> 0
+        self.assertEqual(self.transformer.transform(self.submissions),
+                         [[1], [0], [0]])
+
+
+if __name__ == '__main__':
+    unittest.main()
