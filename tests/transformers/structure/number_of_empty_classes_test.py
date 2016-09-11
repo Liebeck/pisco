62d84794f9f9ae55e850faa0500613b58e2f862f
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-09-11 23:13:09 +0200

    Add another test case for number of empty classes feature, #68

--- tests/transformers/structure/number_of_empty_classes_test.py
+++ tests/transformers/structure/number_of_empty_classes_test.py
@@ -11,20 +11,22 @@ def read_submission(filename):
 submission5 = read_submission('tests/resources/submission5.txt')
 submission3 = read_submission('tests/resources/submission3.txt')
 submission6 = read_submission('tests/resources/submission6.txt')
+submission7 = read_submission('tests/resources/submission7.txt')
 
 
 class NumberOfEmptyClassesTest(unittest.TestCase):
     def setUp(self):
         self.transformer = number_of_empty_classes
-        self.submissions = [submission5, submission3, submission6]
+        self.submissions = [submission5, submission3, submission6, submission7]
 
     def test_feature_extraction(self):
         # returns the following counts:
         # 1) [[0], [1]] -> 1
         # 2) [[0, 0], [0]] -> 0
         # 3) [[0], [0]] -> 0
+        # 4) [[0], [1], [0]] ->
         self.assertEqual(self.transformer.transform(self.submissions),
-                         [[1], [0], [0]])
+                         [[1], [0], [0], [1]])
 
 
 if __name__ == '__main__':
