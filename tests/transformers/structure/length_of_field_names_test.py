f7cdb5ace22d369374cef3301591d38bc0ff80b1
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-09-13 18:23:00 +0200

    Update test for #45

--- tests/transformers/structure/length_of_field_names_test.py
+++ tests/transformers/structure/length_of_field_names_test.py
@@ -25,7 +25,7 @@ class LengthOfFieldNamesTest(unittest.TestCase):
         # 1) [[[7]], [[0]]] -> [3.5]
         # 2) [[[0]], [[0]]] -> [0]
         # 3) [[[0]], [[0]], [[1]] -> [1.0 / 3]
-        # 4) [[[1,1], [[1,1,3]]] -> [[[2/2], [[5.0 /3]] -> [4.0 / 3]
+        # 4) [[[1], [[1,1,3]]] -> [[[2/2], [[5.0 /3]] -> [4.0 / 3]
         np.testing.assert_almost_equal(
             self.transformer.transform(self.submissions),
             [[3.5], [0], [1.0 / 3], [4.0 / 3]], decimal=2)
