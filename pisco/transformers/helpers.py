ae8327027d6b21c6a9b723842fb15f71ede8be32
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-09-14 09:46:46 +0200

    added functions to compute cosine similarity between texts in helpers

--- pisco/transformers/helpers.py
+++ pisco/transformers/helpers.py
@@ -1,7 +1,30 @@
 import numpy as np
 from javalang import tokenizer
 import re
+import math
+from collections import Counter
 
+
+WORD = re.compile(r'\w+')
+
+
+def cosine(vec1, vec2):
+    intersection = set(vec1.keys()) & set(vec2.keys())
+    numerator = sum([vec1[x] * vec2[x] for x in intersection])
+
+    sum1 = sum([vec1[x]**2 for x in vec1.keys()])
+    sum2 = sum([vec2[x]**2 for x in vec2.keys()])
+    denominator = math.sqrt(sum1) * math.sqrt(sum2)
+
+    if not denominator:
+        return 0.0
+    else:
+        return float(numerator) / denominator
+
+
+def vector(text):
+    words = WORD.findall(text)
+    return Counter(words)
 
 TOKENIZER_CACHE = {}
 
