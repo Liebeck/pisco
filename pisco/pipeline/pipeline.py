7e1a07a5c424b2fd81a125cf23d3816d735c8141
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-09-16 14:20:51 +0200

    added feature selection to the pipeline

--- pisco/pipeline/pipeline.py
+++ pisco/pipeline/pipeline.py
@@ -1,5 +1,9 @@
 from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.feature_selection import SelectKBest, chi2
 
 
-def pipeline(transformers=None, recognizer=None):
-    return Pipeline(steps=[('union', FeatureUnion(transformers)), recognizer])
+def pipeline(transformers=None, recognizer=None, number_of_features=None):
+    return Pipeline(steps=[('union', FeatureUnion(transformers)),
+                           ('feature_selection',
+                            SelectKBest(chi2, k=number_of_features)),
+                           recognizer])
