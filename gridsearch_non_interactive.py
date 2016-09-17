e81ebb8500e95c1b62b593ec9079fa8c13295546
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-09-17 13:12:43 +0200

    Undo comment

--- gridsearch_non_interactive.py
+++ gridsearch_non_interactive.py
@@ -144,31 +144,31 @@ with open(output_filename, 'w') as outfile:
 
 
 print '***Number of features: {}'.format(number_features)
-# for features in FEATURES:
-#     transformers = map(lambda f: f[1].build(), features)
-#     p = pipeline.pipeline(transformers=transformers,
-#                           recognizer=recognizer[1].build(),
-#                          number_of_features=number_features)
-#     param_grid = OrderedDict()
-#     param_grid.update(recognizer[1].param_grid())
-#     for name, f in features:
-#         param_grid.update(f.param_grid())
-#     scoring = make_score_function(SCORE)
-#     grid_search = GridSearchCV(p, param_grid=param_grid, verbose=50,
-#                                cv=10, n_jobs=NJOBS, scoring=scoring)
-#     grid_search.fit(X, Y)
-#     # scoring API always maximizes the score, so scores which
-#     # need to be minimized are negated in order for the unified
-#     # scoring API to work correctly
-#     best_score = abs(grid_search.best_score_)
-#     print '***Best score: {}'.format(best_score)
-#     best_params = grid_search.best_params_
-#     scorer = grid_search.scorer_
-#     key = str(number_features) + 'features'
-#     result[key] = {}
-#     result[key]['best_score'] = best_score
-#     result[key]['best_params'] = best_params
-#     result[key]['scorer'] = SCORE
-#     print(result)
-#     with open(output_filename, 'w') as outfile:
-#         json.dump(result, outfile, indent=2)
+for features in FEATURES:
+    transformers = map(lambda f: f[1].build(), features)
+    p = pipeline.pipeline(transformers=transformers,
+                          recognizer=recognizer[1].build(),
+                         number_of_features=number_features)
+    param_grid = OrderedDict()
+    param_grid.update(recognizer[1].param_grid())
+    for name, f in features:
+        param_grid.update(f.param_grid())
+    scoring = make_score_function(SCORE)
+    grid_search = GridSearchCV(p, param_grid=param_grid, verbose=50,
+                               cv=10, n_jobs=NJOBS, scoring=scoring)
+    grid_search.fit(X, Y)
+    # scoring API always maximizes the score, so scores which
+    # need to be minimized are negated in order for the unified
+    # scoring API to work correctly
+    best_score = abs(grid_search.best_score_)
+    print '***Best score: {}'.format(best_score)
+    best_params = grid_search.best_params_
+    scorer = grid_search.scorer_
+    key = str(number_features) + 'features'
+    result[key] = {}
+    result[key]['best_score'] = best_score
+    result[key]['best_params'] = best_params
+    result[key]['scorer'] = SCORE
+    print(result)
+    with open(output_filename, 'w') as outfile:
+        json.dump(result, outfile, indent=2)
